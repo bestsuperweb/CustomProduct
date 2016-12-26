3fa9f077ea9484480c428f591c5aafba648d712f
Tom <coolplum@coolplums-iMac.local>
2016-12-26 12:30:00 -0500

    Back

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,5 +3,4 @@ ShopifyApp.configure do |config|
   config.secret = "a0cf8803c66b8a0fba4b46d4baf8c0a0"
   config.scope = "read_orders, write_products, read_products, read_themes, write_themes"
   config.embedded_app = true
-  config.pos_app = true
 end
