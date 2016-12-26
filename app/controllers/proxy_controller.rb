9bbc8b9c9aa869b8a168a640beb30f7e861f5c37
Tom <coolplum@coolplums-iMac.local>
2016-12-26 16:49:46 -0500

    Modify

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -8,6 +8,26 @@ class ProxyController < ShopifyApp::AuthenticatedController
     puts params
   end
 
+  def _prepare_option(product, option_data)
+      # todo performance
+
+      if option_data['preview_type'] == 'Img'
+        preview_img = product.images.find { |i| i.id == option_data['preview_data']['value'] }
+        if preview_img
+          option_data['preview_data']['value'] = preview_img.attributes[:src]
+        end
+      end
+
+      if option_data['option_type'] == 'Img'
+        preview_img = product.images.find { |i| i.id == option_data['option_data']['value'] }
+        if preview_img
+          option_data['option_data']['value'] = preview_img.attributes[:src]
+        end
+      end
+
+      return option_data
+    end
+
   def client
     @id = params[:product_id]
     puts ">>>>>>> customizer"
